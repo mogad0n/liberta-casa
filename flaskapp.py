8c02fd25f68bad1df0931ca95af972be3eaad0d4
mogad0n <melodon@tuta.io>
2020-05-24 18:40:35 +0530

    Updated Working ircbot and performed basic linkage with flaskapp

--- flaskapp.py
+++ flaskapp.py
@@ -1,9 +1,10 @@
 from flask import Flask, render_template, url_for, request, redirect
 from forms import RegistrationForm
+from irc_register import ircregister
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '189aee7e774aaedce1269c5a35966db7'
+app.config['SECRET_KEY'] = '189aee7e774aaedce1269c5a35966db7' #remove later
 
 @app.route('/')
 def hello():
@@ -17,8 +18,17 @@ def register():
 
         username = request.form.get('username')
         password = request.form.get('password')
-        print(username)
-        print(password)
+        ircregister(username, password)
+        response = ircregister()
+        if response == "server failure":
+            print("x") #add a redirect to a different route?
+        elif response == "433":
+            print("Please select a different Nick") # Figure out how to deliver this response. popups suck.
+        elif response == "success":
+            print("Successfull Registration") # add a redirect to the home page?
+        elif response == "failure":
+            print("Unknown Error") # Try later...
+
     return render_template('register.html', title='Register', form=form)
 
 if __name__ == '__main__':
---
+++ irc_register.py
@@ -0,0 +1,47 @@
+import socket, irctokens
+
+
+def ircregister(username, password):
+    # define the variables
+    d = irctokens.StatefulDecoder()
+    e = irctokens.StatefulEncoder()
+    s = socket.socket()
+
+    #connecting to the server
+    s.connect(("127.0.0.1", 6667))
+
+    #defining the send function with proper formatting
+    def _send(line):
+        print(f"> {line.format()}")
+        e.push(line)
+        while e.pending():
+            e.pop(s.send(e.pending()))
+
+    # registering the connection to the server
+
+    _send(irctokens.build("USER", [username, "0", "*", username]))
+    _send(irctokens.build("NICK", [username]))
+
+    # go through the cases
+
+    while True:
+        lines = d.push(s.recv(1024))
+
+        if lines == None:    #if nothing is received from server
+            return "server error"
+            break
+
+        for line in lines:
+            print(f"< {line.format()}")
+
+            if line.command == "433": # if nickname already in use
+                return "433"
+
+            elif line.command == "005": # when 005 is received pass the nickserv register command command
+                _send(irctokens.build("PRIVMSG", ["NickServ", f"REGISTER {password}"]))
+
+            if line.command == "NOTICE" and line.params == [username, "Account created"]: # if Services respond with appropriate notice NOTICE
+                _send(irctokens.build("QUIT"))
+                return "success"
+
+# register("hello", "test")
