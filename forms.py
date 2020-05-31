82f5869ed270a774cdf9c6cf5dbf3e980ca43959
mogad0n <melodon@tuta.io>
2020-05-31 20:42:00 +0530

    Add email field and verification class

--- forms.py
+++ forms.py
@@ -5,6 +5,11 @@ from wtforms.validators import DataRequired, Length, EqualTo
 
 class RegistrationForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired(), Length(min=1, max=32)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])
     confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Register')
+
+class VerificationForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min=1, max=32)])
+    verif_code = StringField('Verification Code', validators=[DataRequired()])
