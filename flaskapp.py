9a46555abdcd4b27d798f8b8a0aac3ac77d5021c
mogad0n <melodon@tuta.io>
2020-05-31 21:41:27 +0530

    fixed imports and temporarily disabled a route

--- flaskapp.py
+++ flaskapp.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, url_for, request, redirect, flash
-from forms import RegistrationForm
+from forms import RegistrationForm, VerificationForm
 from irc_register import ircregister
 from irc_verify import ircverify
 
